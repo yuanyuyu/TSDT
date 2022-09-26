740031e9ff3c39b125454fe19e23ef2704dc3e54
yyy <yuanyuyu@bupt.edu.cn>
2022-09-26 16:31:13 +0800

    message 5: The first iterative developmen

--- lists/tests.py
+++ lists/tests.py
@@ -1,12 +1,22 @@
 from django.test import TestCase
 from django.urls import resolve
-from lists.views import home_page 
+from lists.views import home_page
+from django.http import HttpRequest
 
 class HomePageTest(TestCase):
 
 	def test_root_url_resolves_to_home_page_view(self):
 		found = resolve('/')      
-		self.assertEqual(found.func, home_page)  
+		self.assertEqual(found.func, home_page)
+
+	def test_home_page_return_correct_html(self):
+		request = HttpRequest()  
+		response = home_page(request) 
+		html = response.content.decode('utf8')
+		self.assertTrue(html.startswith('<html>'))  
+		self.assertIn('<title>To-Do lists</title>', html) 
+		self.assertTrue(html.endswith('</html>')) 
+
 
 
 
