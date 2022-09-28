98c0cb324ccce1e6be783e03b84ffef0a486ce1b
yyy <yuanyuyu@bupt.edu.cn>
2022-09-28 15:58:56 +0800

    message 6: Functional test now checks we can input a to-do item

--- functional_tests.py
+++ functional_tests.py
@@ -1,5 +1,8 @@
-from selenium import webdriver  
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time  
 import unittest
+from selenium.webdriver.common.by import By
 
 
 class NewVisitorTest(unittest.TestCase): 
@@ -17,18 +20,32 @@ class NewVisitorTest(unittest.TestCase):
 		self.browser.get('http://localhost:8000') 
 
 		# 他注意到网页的标题和头部都包含“To-Do"这个词          
-		self.assertIn('To-Do', self.browser.title) 
-		self.fail('Finish the test!') 
-
+		self.assertIn('To-Do', self.browser.title)
+		header_text = self.browser.find_element(By.TAG_NAME,'h1').text
+		self.assertIn('To-Do', header_text)
+		
 		# 应用有一个输入待办事项的文本输入框
+		inputbox = self.browser.find_element(By.ID,'id_new_item')
+		self.assertEqual(
+			inputbox.get_attribute('placeholder'),
+			'Enter a to-do item'
+			)
 
 		# 他在文本输入框中输入了“Buy flowers"
+		inputbox.send_keys('Buy flowers')
 
 		# 他按了回车键键后，页面更新了
-		# 待办事项表格中显示了“1：Buy flowers"  
+		# 待办事项表格中显示了“1：Buy flowers" 
+		inputbox.send_keys(Keys.ENTER)
+		time.sleep(1) 
+
+		table = self.browser.find_element(By.ID,'id_list_table')
+		rows = table.find_elements(By.TAG_NAME,'tr')
+		self.assertIn('1: Buy flowers', [row.text for row in rows])
 
 		# 页面中又显示了一个文本输入框，可以输入其他待办事项
 		# 他输入了“gift to girlfriend" 
+		self.fail('Finish the test!') 
 
 		# 页面再次更新，她的清单中显示了这两个待办事项
 
--- lists/tests.py
+++ lists/tests.py
@@ -10,12 +10,12 @@ class HomePageTest(TestCase):
 		self.assertEqual(found.func, home_page)
 
 	def test_home_page_return_correct_html(self):
-		request = HttpRequest()  
-		response = home_page(request) 
-		html = response.content.decode('utf8')
-		self.assertTrue(html.startswith('<html>'))  
-		self.assertIn('<title>To-Do lists</title>', html) 
-		self.assertTrue(html.endswith('</html>')) 
+		request = HttpRequest()  #(1)
+		response = home_page(request) #(2)
+		html = response.content.decode('utf8') #(3)
+		self.assertTrue(html.startswith('<html>'))  #(4)
+		self.assertIn('<title>To-Do lists</title>', html)  #(5)
+		self.assertTrue(html.endswith('</html>'))  #(4)
 
 
 
