9893caf34552942c2102d310c1ad8e2cbb4931b6
yyy <yuanyuyu@bupt.edu.cn>
2022-10-04 16:42:02 +0800

    message 7: Refactoring

--- functional_tests.py
+++ functional_tests.py
@@ -21,26 +21,26 @@ class NewVisitorTest(unittest.TestCase):
 
 		# 他注意到网页的标题和头部都包含“To-Do"这个词          
 		self.assertIn('To-Do', self.browser.title)
-		header_text = self.browser.find_element(By.TAG_NAME,'h1').text
+		header_text = self.browser.find_element(By.TAG_NAME,'h1').text  #(1)
 		self.assertIn('To-Do', header_text)
 		
 		# 应用有一个输入待办事项的文本输入框
-		inputbox = self.browser.find_element(By.ID,'id_new_item')
+		inputbox = self.browser.find_element(By.ID,'id_new_item') #(1)
 		self.assertEqual(
 			inputbox.get_attribute('placeholder'),
 			'Enter a to-do item'
 			)
 
 		# 他在文本输入框中输入了“Buy flowers"
-		inputbox.send_keys('Buy flowers')
+		inputbox.send_keys('Buy flowers') #(2)
 
 		# 他按了回车键键后，页面更新了
 		# 待办事项表格中显示了“1：Buy flowers" 
-		inputbox.send_keys(Keys.ENTER)
-		time.sleep(1) 
+		inputbox.send_keys(Keys.ENTER) #(3)
+		time.sleep(1) #(4)
 
 		table = self.browser.find_element(By.ID,'id_list_table')
-		rows = table.find_elements(By.TAG_NAME,'tr')
+		rows = table.find_elements(By.TAG_NAME,'tr') #(1)
 		self.assertIn('1: Buy flowers', [row.text for row in rows])
 
 		# 页面中又显示了一个文本输入框，可以输入其他待办事项
--- lists/tests.py
+++ lists/tests.py
@@ -2,6 +2,7 @@ from django.test import TestCase
 from django.urls import resolve
 from lists.views import home_page
 from django.http import HttpRequest
+from django.template.loader import render_to_string
 
 class HomePageTest(TestCase):
 
@@ -10,12 +11,8 @@ class HomePageTest(TestCase):
 		self.assertEqual(found.func, home_page)
 
 	def test_home_page_return_correct_html(self):
-		request = HttpRequest()  #(1)
-		response = home_page(request) #(2)
-		html = response.content.decode('utf8') #(3)
-		self.assertTrue(html.startswith('<html>'))  #(4)
-		self.assertIn('<title>To-Do lists</title>', html)  #(5)
-		self.assertTrue(html.endswith('</html>'))  #(4)
+		response = self.client.get('/')
+		self.assertTemplateUsed(response, 'home.html')
 
 
 
