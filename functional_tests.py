c3ceb54602b8e7cf2b047e7d2503a3e132389a36
yyy <yuanyuyu@bupt.edu.cn>
2022-10-08 17:04:25 +0800

    message 8 :Before refactoring the functional_tests

--- functional_tests.py
+++ functional_tests.py
@@ -44,13 +44,21 @@ class NewVisitorTest(unittest.TestCase):
 		self.assertIn('1: Buy flowers', [row.text for row in rows])
 
 		# 页面中又显示了一个文本输入框，可以输入其他待办事项
-		# 他输入了“gift to girlfriend" 
-		self.fail('Finish the test!') 
-
+		# 他输入了“Give a gift to Lisi" 
+		inputbox = self.browser.find_element(By.ID,'id_new_item')
+		inputbox.send_keys('Give a gift to Lisi')
+		inputbox.send_keys(Keys.ENTER)
+		time.sleep(1)
+		
 		# 页面再次更新，她的清单中显示了这两个待办事项
+		table = self.browser.find_element(By.ID,'id_list_table')
+		rows = table.find_elements(By.TAG_NAME, 'tr')
+		self.assertIn('1: Buy flowers', [row.text for row in rows])
+		self.assertIn('2: Give a gift to Lisi', [row.text for row in rows])
 
 		# 张三想知道这个网站是否会记住她的清单
 		# 他看到网站为他生成了一个唯一的URL
+		self.fail('Finish the test!') 
 
 		# 他访问那个URL，发现他的待办事项列表还在
 		# 他满意的离开了
