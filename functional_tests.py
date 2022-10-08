3aa43f942779a9ec99571ed589846947716fc1d5
yyy <yuanyuyu@bupt.edu.cn>
2022-10-08 17:18:16 +0800

    message 9: After refactoring the functional_tests

--- functional_tests.py
+++ functional_tests.py
@@ -13,6 +13,12 @@ class NewVisitorTest(unittest.TestCase):
 	def tearDown(self): 
 		self.browser.quit()
 
+	def check_for_row_in_list_table(self, row_text):
+		table = self.browser.find_element(By.ID,'id_list_table')
+		rows = table.find_elements(By.TAG_NAME,'tr') 
+		self.assertIn(row_text, [row.text for row in rows])
+
+
 	def test_can_start_a_list_and_retrieve_it_later(self): 
 
 		# 张三听说有一个在线待办事项的应用
@@ -38,10 +44,9 @@ class NewVisitorTest(unittest.TestCase):
 		# 待办事项表格中显示了“1：Buy flowers" 
 		inputbox.send_keys(Keys.ENTER) #(3)
 		time.sleep(1) #(4)
+		self.check_for_row_in_list_table('1: Buy flowers')
 
-		table = self.browser.find_element(By.ID,'id_list_table')
-		rows = table.find_elements(By.TAG_NAME,'tr') #(1)
-		self.assertIn('1: Buy flowers', [row.text for row in rows])
+		
 
 		# 页面中又显示了一个文本输入框，可以输入其他待办事项
 		# 他输入了“Give a gift to Lisi" 
@@ -51,10 +56,8 @@ class NewVisitorTest(unittest.TestCase):
 		time.sleep(1)
 		
 		# 页面再次更新，她的清单中显示了这两个待办事项
-		table = self.browser.find_element(By.ID,'id_list_table')
-		rows = table.find_elements(By.TAG_NAME, 'tr')
-		self.assertIn('1: Buy flowers', [row.text for row in rows])
-		self.assertIn('2: Give a gift to Lisi', [row.text for row in rows])
+		self.check_for_row_in_list_table('1: Buy flowers')
+		self.check_for_row_in_list_table('2: Give a gift to Lisi')
 
 		# 张三想知道这个网站是否会记住她的清单
 		# 他看到网站为他生成了一个唯一的URL
