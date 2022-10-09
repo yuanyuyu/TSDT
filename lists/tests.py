3762fde660c46e2b50273f712479ff9de57dcf2b
yyy <yuanyuyu@bupt.edu.cn>
2022-10-09 15:11:12 +0800

    message 10: First model migration

--- lists/tests.py
+++ lists/tests.py
@@ -3,12 +3,28 @@ from django.urls import resolve
 from lists.views import home_page
 from django.http import HttpRequest
 from django.template.loader import render_to_string
+from lists.models import Item
+
+class ItemModelTest(TestCase):
+
+	def test_saving_and_retrieving_items(self):
+		first_item = Item()
+		first_item.text = 'The first list item'
+		first_item.save()
+
+		second_item = Item()
+		second_item.text = 'Item the second'
+		second_item.save()
+
+		saved_items = Item.objects.all()
+		self.assertEqual(saved_items.count(), 2)
+
+		first_saved_item = saved_items[0]
+		second_saved_item = saved_items[1]
+		self.assertEqual(first_saved_item.text, 'The first list item')
+		self.assertEqual(second_saved_item.text, 'Item the second')
 
 class HomePageTest(TestCase):
-
-	def test_root_url_resolves_to_home_page_view(self):
-		found = resolve('/')      
-		self.assertEqual(found.func, home_page)
 
 	def test_uses_home_template(self):
 		response = self.client.get('/')
