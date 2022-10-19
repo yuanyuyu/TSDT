5942d96a9fd15820de83912edee5a593e64c03b6
yyy <yuanyuyu@bupt.edu.cn>
2022-10-19 16:55:36 +0800

    message 14: add a new user

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -27,7 +27,7 @@ class NewVisitorTest(LiveServerTestCase):
 			except (AssertionError, WebDriverException) as e: #(5)
 				if time.time() - start_time > MAX_WAIT: #(6)
 					raise e #(6)
-				time.sleep(0.5) #(5)
+				time.sleep(0.5) 
 
 
 	def test_can_start_a_list_and_retrieve_it_later(self): 
@@ -68,12 +68,51 @@ class NewVisitorTest(LiveServerTestCase):
 		self.wait_for_row_in_list_table('1: Buy flowers')
 		self.wait_for_row_in_list_table('2: Give a gift to Lisi')
 
-		# 张三想知道这个网站是否会记住她的清单
-		# 他看到网站为他生成了一个唯一的URL
-		self.fail('Finish the test!') 
-
-		# 他访问那个URL，发现他的待办事项列表还在
 		# 他满意的离开了
 
-if __name__ == '__main__': 
-	unittest.main()
+	def test_multiple_users_can_start_lists_at_different_urls(self):
+		# 张三新建一个待办事项清单
+		self.browser.get(self.live_server_url) 
+		inputbox = self.browser.find_element(By.ID,'id_new_item') 
+		inputbox.send_keys('Buy flowers') 
+		inputbox.send_keys(Keys.ENTER) 
+		self.wait_for_row_in_list_table('1: Buy flowers')
+
+		# 他注意到清单有个唯一的URL
+		zhangsan_list_url = self.browser.current_url
+		self.assertRegex(zhangsan_list_url, '/lists/.+') 
+
+		# 现在一个新用户王五访问网站
+		# 我们使用一个新浏览器会话
+		#确保张三的信息不会从cookie中泄露出去
+		self.browser.quit()
+		self.browser = webdriver.Chrome()
+
+		# 王五访问首页
+		# 页面中看不到张三的清单
+		self.browser.get(self.live_server_url)
+		page_text = self.browser.find_element(By.TAG_NAME,'body').text
+		self.assertNotIn('Buy flowers', page_text)
+		self.assertNotIn('BGive a gift to Lisi', page_text)
+
+		# 王五输入一个新待办事项，新建一个清单
+		inputbox = self.browser.find_element(By.ID, 'id_new_item')
+		inputbox.send_keys('Buy milk')
+		inputbox.send_keys(Keys.ENTER)
+		self.wait_for_row_in_list_table('1: Buy milk')
+
+		# 王五获得了他的唯一URL
+		wangwu_list_url = self.browser.current_url
+		self.assertRegex(wangwu_list_url, '/lists/.+')
+		self.assertNotEqual(wangwu_list_url, zhangsan_list_url)
+
+		# 这个页面还是没有张三的清单
+		page_text = self.browser.find_element(By.TAG_NAME,'body').text
+		self.assertNotIn('Buy flowers', page_text)
+		self.assertIn('Buy milk', page_text)
+
+		# 两人都很满意，然后去睡觉了
+
+
+
+
