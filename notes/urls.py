07b9c0891ede1397b920dd80b3fd04e85328cfbc
yyy <yuanyuyu@bupt.edu.cn>
2022-10-25 17:39:59 +0800

    message 16: form URL of new list by POST

--- notes/urls.py
+++ notes/urls.py
@@ -19,5 +19,6 @@ from lists import views
 
 urlpatterns = [
     path('', views.home_page, name='home'),
-    path('lists/the-new-page/',views.view_list, name='view_list')
+    path('lists/new', views.new_list, name='new_list'),
+    path('lists/the-new-page/',views.view_list, name='view_list'),
 ]
