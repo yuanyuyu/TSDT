673fe79a1f45d418a631d0e171354b721b211252
yyy <yuanyuyu@bupt.edu.cn>
2022-10-29 16:38:09 +0800

    message 17: change models for multiple users

--- lists/tests.py
+++ lists/tests.py
@@ -3,18 +3,26 @@ from django.urls import resolve
 from lists.views import home_page
 from django.http import HttpRequest
 from django.template.loader import render_to_string
-from lists.models import Item
+from lists.models import Item, List
 
-class ItemModelTest(TestCase):
+class ListAndItemModelsTest(TestCase):
 
 	def test_saving_and_retrieving_items(self):
+		list_user = List()
+		list_user.save()
+
 		first_item = Item()
 		first_item.text = 'The first list item'
+		first_item.list = list_user
 		first_item.save()
 
 		second_item = Item()
 		second_item.text = 'Item the second'
+		second_item.list = list_user
 		second_item.save()
+
+		saved_list = List.objects.first()
+		self.assertEqual(saved_list, list_user)
 
 		saved_items = Item.objects.all()
 		self.assertEqual(saved_items.count(), 2)
@@ -22,7 +30,9 @@ class ItemModelTest(TestCase):
 		first_saved_item = saved_items[0]
 		second_saved_item = saved_items[1]
 		self.assertEqual(first_saved_item.text, 'The first list item')
+		self.assertEqual(first_saved_item.list, list_user)
 		self.assertEqual(second_saved_item.text, 'Item the second')
+		self.assertEqual(second_saved_item.list, list_user)
 
 class HomePageTest(TestCase):
 
@@ -37,8 +47,9 @@ class ListViewTest(TestCase):
 		self.assertTemplateUsed(response, 'list.html')
 
 	def test_displays_all_list_items(self):
-		Item.objects.create(text='itemey 1')
-		Item.objects.create(text='itemey 2')
+		list_user = List.objects.create()
+		Item.objects.create(text='itemey 1', list=list_user)
+		Item.objects.create(text='itemey 2', list=list_user)
 
 		response = self.client.get('/lists/the-new-page/')
 
--- lists/views.py
+++ lists/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from lists.models import Item
+from lists.models import Item, List
+
+def new_list(request):
+	list_user = List.objects.create()
+	Item.objects.create(text=request.POST['item_text'], list=list_user) 
+	return redirect('/lists/the-new-page/')
 
 def home_page(request):
 	return render(request, 'home.html')
@@ -9,8 +14,5 @@ def view_list(request):
 	items = Item.objects.all()	
 	return render(request, 'list.html', {'items': items})
 
-def new_list(request):
-	Item.objects.create(text=request.POST['item_text']) 
-	return redirect('/lists/the-new-page/')
 
 # Create your views here.
