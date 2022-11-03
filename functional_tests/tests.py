fe41fdc845eb289fe9d0d84cfb6fb90b216a15af
yyy <yuanyuyu@bupt.edu.cn>
2022-11-03 15:40:20 +0800

    message20: testing fot layout + styling

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -113,6 +113,32 @@ class NewVisitorTest(LiveServerTestCase):
 
 		# 两人都很满意，然后去睡觉了
 
+	def test_layout_and_styling(self):
+		# 张三访问首页
+		self.browser.get(self.live_server_url)
+		self.browser.set_window_size(1024, 768)
+
+		# 他看到输入框完美地居中显示
+		inputbox = self.browser.find_element(By.ID, 'id_new_item')
+		self.assertAlmostEqual(
+			inputbox.location['x'] + inputbox.size['width'] / 2,
+			512,
+			delta=10
+			)
+
+		# 他新建一个清单，看到输入框仍然完美的居中显示
+		inputbox.send_keys('testing')
+		inputbox.send_keys(Keys.ENTER)
+		self.wait_for_row_in_list_table('1: testing')
+		inputbox = self.browser.find_element(By.ID, 'id_new_item')
+		self.assertAlmostEqual(
+			inputbox.location['x'] + inputbox.size['width'] / 2,
+			512,
+			delta=10
+			)
+
+
+
 
 
 
